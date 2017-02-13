334f18950edf6f705f574c434f2c36d9fe444601
fanatikska <studenov89@yandex.ru>
2017-02-13 15:53:39 +0300

    Вторая часть первого задания

--- sandbox/src/main/java/ru/java/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/java/sandbox/FirstProgram.java
@@ -2,8 +2,18 @@ package ru.java.sandbox;
 
 public class FirstProgram {
 
-public static void main (String[] args) {
+    public static void main(String[] args) {
 
-System.out.println("Hello");
-}
+        System.out.println("Hello");
+        Point point1 = new Point(2, 2);
+        Point point2 = new Point(4, 4);
+        System.out.println(distance(point1, point2));
+        System.out.println(point1.distance());
+    }
+
+    public static double distance(Point p1, Point p2) {
+
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+    }
+
 }
---
+++ sandbox/src/main/java/ru/java/sandbox/Point.java
@@ -0,0 +1,19 @@
+package ru.java.sandbox;
+
+/**
+ * Created by Studenov-DV on 13.02.2017.
+ */
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance() {
+
+        return Math.sqrt(Math.pow((this.x - this.x), 2) + Math.pow((this.y - this.y), 2));
+    }
+}
