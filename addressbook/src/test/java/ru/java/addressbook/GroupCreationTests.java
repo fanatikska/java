aa62211794144caa156e372e4018895c3efb2c42
fanatikska <studenov89@yandex.ru>
2017-02-18 17:02:17 +0300

    Выделение вспомогательных методов

--- addressbook/src/test/java/ru/java/addressbook/GroupCreationTests.java
+++ addressbook/src/test/java/ru/java/addressbook/GroupCreationTests.java
@@ -22,6 +22,10 @@ public class GroupCreationTests {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost:8080/addressbook/");
+        login();
+    }
+
+    private void login() {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
@@ -30,12 +34,22 @@ public class GroupCreationTests {
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
-    
+
     @Test
     public void testGroupCreation() {
 
-        wd.findElement(By.linkText("groups")).click();
-        wd.findElement(By.name("new")).click();
+        goToGroupPage();
+        initGroupCreation();
+        fillGroupForm();
+        submitGroupCreation();
+        goToGroupPage();
+    }
+
+    private void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void fillGroupForm() {
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
         wd.findElement(By.name("group_name")).sendKeys("stest1");
@@ -45,10 +59,16 @@ public class GroupCreationTests {
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
         wd.findElement(By.name("group_footer")).sendKeys("stest3");
-        wd.findElement(By.name("submit")).click();
+    }
+
+    private void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    private void goToGroupPage() {
         wd.findElement(By.linkText("groups")).click();
     }
-    
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
