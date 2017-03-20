e93fa4b151bdd91b5a327ab268579d2164d70202
fanatikska <studenov89@yandex.ru>
2017-03-20 16:58:47 +0300

    Добавлено считывание из файла для создания групп

--- addressbook/src/test/java/ru/java/addressbook/tests/GroupCreationTest.java
+++ addressbook/src/test/java/ru/java/addressbook/tests/GroupCreationTest.java
@@ -3,13 +3,13 @@ package ru.java.addressbook.tests;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.java.addressbook.model.GroupData;
 import ru.java.addressbook.model.Groups;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -17,12 +17,25 @@ import static org.testng.Assert.*;
 
 public class GroupCreationTest extends TestBase{
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups() throws IOException {
+        List<Object[]> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while(line != null){
+            String[] split = line.split(";");
+            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
 
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("stest1").withHeader("step2").withFooter("stest3");
+     //   GroupData group = new GroupData().withName("stest1").withHeader("step2").withFooter("stest3");
         app.group().create(group);
         app.goTo().groupPage();
         assertThat(app.group().count(), equalTo(before.size() + 1));
