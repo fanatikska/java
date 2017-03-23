7741a78bc4939c554e225aaf69f443206a66c413
fanatikska <studenov89@yandex.ru>
2017-03-23 13:58:49 +0300

    Мелкие улучшения

--- addressbook/src/test/java/ru/java/addressbook/tests/TestBase.java
+++ addressbook/src/test/java/ru/java/addressbook/tests/TestBase.java
@@ -9,7 +9,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.java.addressbook.appmanager.ApplicationManager;
 
-import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
